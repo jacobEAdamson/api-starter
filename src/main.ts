a46d23d4074719351bf66f993b4180b6ea5c1d71
jacobEAdamson <jacob.e.adamson@gmail.com>
2024-12-30 11:24:17 -0500

    Improve test coverage (#3)
    Adds additional tests to raise the overall code coverage of the repo

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await (await app.resolve('sequelize')).sync({ force: true });
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/user.controller.spec.ts
@@ -0,0 +1,57 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { UserController } from './user.controller';
+import { Sequelize } from 'sequelize-typescript';
+import UserModel from './user.model';
+
+describe('UserController', () => {
+  let app: TestingModule;
+  let userController: UserController;
+  let sequelize: Sequelize;
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    userController = app.get(UserController);
+    sequelize = app.get('sequelize');
+    await sequelize.sync({ force: true });
+  });
+
+  describe('index', () => {
+    it('should return an empty list when no records', async () => {
+      expect(JSON.parse(await userController.index())).toEqual([]);
+    });
+
+    it('should return ids of records when they exist', async () => {
+      const userModel = sequelize.getRepository(UserModel);
+      userModel.create({ name: 'Jacob Adamson' });
+      const result = JSON.parse(await userController.index());
+      expect(result.find((o) => o['name'] == 'Jacob Adamson')).toBeTruthy();
+    });
+  });
+
+  describe('create', () => {
+    it('should allow creating a user with the correct paramters', async () => {
+      const result = JSON.parse(
+        await userController.create({
+          name: 'Jacob Adamson',
+        }),
+      );
+      expect(result['result']).toEqual('success');
+    });
+
+    it('should properly validate', async () => {
+      const result = JSON.parse(
+        await userController.create({
+          name: '',
+        }),
+      );
+      expect(result['result']).toEqual('error');
+      expect(result['errors'][0]['message']).toContain(
+        'Validation notEmpty on name failed',
+      );
+    });
+  });
+});
